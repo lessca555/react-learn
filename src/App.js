607b0e5b3b8bdc09e2dc0621af940de52c95eea7
Christian Samuel Seputra <stegenvak@gmail.com>
2022-12-13 12:06:30 +0700

    awesome

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           </div>
           <div className='card-body'>
             <h5>Card body</h5>
+            <i class="fa-brands fa-facebook"></i>
           </div>
         </div>
 
